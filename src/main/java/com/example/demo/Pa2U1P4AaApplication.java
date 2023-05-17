ee617c82969927876a85964ab57189fc664109e5
Alex Andrango <arandrangoa@uce.edu.ec>
2023-05-16 19:42:12 -0500

    Taller 5

--- src/main/java/com/example/demo/Pa2U1P4AaApplication.java
+++ src/main/java/com/example/demo/Pa2U1P4AaApplication.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Pa2U1P4AaApplication implements CommandLineRunner{
+	
+	@Autowired
+	private Profesor prof2;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P4AaApplication.class, args);
@@ -21,9 +26,25 @@ public class Pa2U1P4AaApplication implements CommandLineRunner{
 		prof.setApellido("Andrango");
 		prof.setCedula("1727193847");
 		prof.setNombre("Alex");
-		prof.setFechaNacimiento(LocalDateTime.now());
+		prof.setFechaNacimiento(LocalDateTime.of(1999, 11, 21, 0, 0));	
 		
 		System.out.println(prof);
+		System.out.println(prof.getApellido());
+		
+		System.out.println(prof2);
+		prof2.setApellido("Teran");
+		System.out.println(prof2.getApellido());
+		
+		Profesor prof3;
+		prof3=prof;
+		System.out.println(prof3);
+		prof.setApellido("Mullo");
+		System.out.println(prof3);
+		
+		
+		
+		MatriculaCalculo mat=new MatriculaCalculo();
+		mat.realizarMatricula("1");
 	}
 
 }
--- src/main/java/com/example/demo/Profesor.java
+++ src/main/java/com/example/demo/Profesor.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import java.time.LocalDateTime;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Profesor {
 	
 	private String nombre;
